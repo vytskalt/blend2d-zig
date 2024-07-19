83f9fbb6a6c18f27316716c12c6b206bdfc0e84d
vytskalt <vytskalt@protonmail.com>
2024-07-19 19:17:29 +0300

    add temporary executable

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 
 [Blend2D](https://blend2d.com/) ported to the Zig build system
 
-thank me later
--- build.zig
+++ build.zig
@@ -1,14 +1,16 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     const upstream_blend2d = b.dependency("blend2d", .{});
     const upstream_asmjit = b.dependency("asmjit", .{});
 
-    const target = b.standardTargetOptions(.{});
     const lib = b.addStaticLibrary(.{
         .name = "blend2d",
         .target = target,
-        .optimize = b.standardOptimizeOption(.{}),
+        .optimize = optimize,
     });
     lib.linkLibC();
     lib.linkLibCpp();
@@ -312,4 +314,25 @@ pub fn build(b: *std.Build) void {
     lib.installHeadersDirectory(upstream_blend2d.path("src"), "", .{});
 
     b.installArtifact(lib);
+
+    // ---
+
+    const exe = b.addExecutable(.{
+        .name = "renderer",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.linkLibrary(lib);
+
+    b.installArtifact(exe);
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
 }
